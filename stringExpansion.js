324b2b89a4a318274b744f4793f674ff7b9bcdd7
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-04 22:13:28 +0300

    stringExpansion is complete and commented, awaiting possible refactoring

--- stringExpansion.js
+++ stringExpansion.js
@@ -3,12 +3,34 @@ const stringExpansion = function multiplyLettersByNumeric(string) {
   // function is immutable
   let workString = string;
 
-  const numericRegExp = /\d(?=\D)\D|\d(?!\D)|(?<!\d)\D/g;
+  // regexp that matches pairs of number-letter and lone letters, but don't match lone numbers
+  const splitRegExp = /\d(?=\D)\D|(?<!\d)\D/g;
 
-  let numerics = workString.match(numericRegExp);
+  // string to return
+  let resultString = '';
 
-  console.log(numerics);
+  // splitting the string into array of pairs and lone letters, abandoning "stray" numericals
+  workString = workString.match(splitRegExp);
 
+  for(let i = 0; i < workString.length; i++) {
+    
+    
+    if(workString[i].length > 1) {
+
+    /*
+    * If there is more than one char in the element of the array
+    * We take the char at second position of the pair (index 1)
+    * While parsing the first one (index 0) as integer
+    * And then we create a string of repeating second-position chars via "repeat" method 
+    * Finally we concat this string with our resulting string
+    */
+      resultString = resultString.concat(workString[i].charAt(1).repeat(parseInt(workString[i].charAt(0), 10)));
+    } else {
+
+      // if there is only one char, we just concat it with the resulting string
+      resultString = resultString.concat(workString[i]);
+    }
+  }
+
+  return resultString;
 }
-
-stringExpansion('abcde');
