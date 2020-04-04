ebae341f362e59319d3f81edbd20e38bdc06ec7a
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-04 19:23:14 +0300

    toCamelCase function is complete and commented, awaiting possible refactoring

--- toCamelCase.js
+++ toCamelCase.js
@@ -3,15 +3,29 @@ const toCamelCase = function dashedUnderscoredToCamelCase(string) {
   // function is immutable
   let workString = string;
 
-  console.log(workString);
 
-  const regExp = /[-|_]/g;
+  // regular expression to find dash and underscore in the string
+  const dashUnderscoreRegExp = /_|-/g;
 
-  workString.replace(regExp, ' ');
 
-  console.log(workString);
+  // replacing all the dashes and underscores with space to utilize separators
+  workString = workString.replace(dashUnderscoreRegExp, ' ');
+  workString = workString.split(' ');
+
+  for(let i = 1; i < workString.length; i++) {
+
+    /*
+    * Firstly: we ignore the first word
+    * All following words undergo next: with /^\w/ regexp we match the first letter of each word in replace function
+    * Then we copy that letter with charAt(0) and convert it to uppercase
+    * And we change the matched first letter of the word with its' upper counterpart
+    */ 
+    workString[i] = workString[i].replace(/^\w/, workString[i].charAt(0).toUpperCase());
+
+  }
+
+  // merging the result string
+  workString = workString.join('');
+
+  return workString;
 }
-
-console.log('kek');
-
-toCamelCase("the-stealth-warrior");
