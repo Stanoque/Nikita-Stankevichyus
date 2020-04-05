964bb6e1e4e398ad5db6472c8d152ec9b880565d
Stanoque <stanoquedzhumpsh@gmail.com>
2020-04-05 17:50:28 +0300

    largestSmallest function is refactored to utilize "reduce" method

--- Nikita-Stankevichyus/largestSmallest.js
+++ Nikita-Stankevichyus/largestSmallest.js
@@ -1,4 +1,65 @@
 
+// this function converts arguments array-like object into actual array
+const argsIntoArray = function convertArgumentsIntoArray(args) {
+
+  let argsArray = [];
+
+  for(let i = 0; i < args.length; i++) {
+    argsArray.push(args[i]);
+  }
+
+  return argsArray;
+}
+
+
+const largest = function maxNumberAmongArgs() {
+
+  // if there are no arguments, the function will return "null"
+  if (arguments.length !== 0) {
+
+    // converting arguments object into array, so "reduce" method can be applied to it
+    let argsArray = argsIntoArray(arguments);
+
+    // via reduce finding maximum element outta array
+    let maximum = argsArray.reduce(function(element_1, element_2){
+      return element_1 > element_2 ? element_1 : element_2;
+    })
+
+    return maximum;
+
+  } else {
+    return null;
+  }
+}
+
+const smallest = function minNumberAmongArgs() {
+
+  // if there are no arguments, the function will return "null"
+  if (arguments.length !== 0) {
+
+    // converting arguments object into array, so "reduce" method can be applied to it
+    let argsArray = argsIntoArray(arguments);
+
+    // via reduce finding minimum element outta array
+    let minimum = argsArray.reduce(function(element_1, element_2){
+      return element_1 < element_2 ? element_1 : element_2;
+    })
+
+    return minimum;
+
+  } else {
+    return null;
+  }
+
+}
+
+
+
+
+/*
+ * NOTE: This is old conventional solution
+ * It's fairly simple but functional
+*
 const largest = function maxNumberAmongArgs() {
 
   // if there are no arguments, the function will return "null"
@@ -41,5 +102,6 @@ const smallest = function minNumberAmongArgs() {
   } else {
     return null;
   }
-
 }
+*
+*/
